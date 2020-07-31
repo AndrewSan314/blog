a97c4cabdec06800255771de2fa35bdac5e512da
AndrewSan314 <taduchien314@gmail.com>
2020-07-31 15:49:14 +0700

    third commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (TemplateView, ListView, CreateView,
-                                  DetailView, UpdateView, DeleteView)
+                                  DetailView, UpdateView, DeleteView, )
 from blog.forms import PostForm, CommentForm
 from blog.models import Post, Comment
 from django.utils import timezone
@@ -63,15 +63,16 @@ def post_publish(request, pk):
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == 'post':
+    if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = CommentForm()
-    return render(request, 'blog/post_detail.html', {'form': form})
+    return render(request, 'blog/comment_form.html', {'form': form})
 
 
 @login_required
